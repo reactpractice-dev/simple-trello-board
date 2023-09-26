6e9ee88bab64dbba6532bda5a20a58c61985aeec
Corina Udrescu <corinutz@gmail.com>
2023-09-26 08:47:50 +0200

    Add form for adding a card

--- src/AddCardButton.jsx
+++ src/AddCardButton.jsx
@@ -1,11 +1,38 @@
-import { PlusIcon } from "@heroicons/react/20/solid";
+import { PlusIcon, XMarkIcon } from "@heroicons/react/20/solid";
+import { useState } from "react";
 
 const AddCardButton = () => {
+  const [isAdding, setIsAdding] = useState(false);
+
+  if (isAdding) {
+    return (
+      <div className="p-2">
+        <textarea
+          className="border border-gray-300  bg-slate-50 p-2 w-full rounded-lg"
+          placeholder="Enter a title for this card ..."
+        />
+        <div className="flex items-center">
+          <button className="bg-blue-600 hover:bg-blue-700 py-1 px-2 text-white">
+            Add card
+          </button>
+          <button onClick={() => setIsAdding(false)}>
+            <XMarkIcon className="h-6 w-6 text-gray-500" />
+          </button>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <button className="flex hover:bg-slate-300 p-3 m-3 w-[11.5rem]">
-      <PlusIcon className="h-6 w-6 text-blue-500" />
-      Add a card
-    </button>
+    <div className="m-3">
+      <button
+        className="flex hover:bg-slate-300 p-3 w-full"
+        onClick={() => setIsAdding(true)}
+      >
+        <PlusIcon className="h-6 w-6 text-gray-500" />
+        Add a card
+      </button>
+    </div>
   );
 };
 
--- src/Column.jsx
+++ src/Column.jsx
@@ -2,7 +2,7 @@ import AddCardButton from "./AddCardButton";
 
 const Column = ({ title }) => {
   return (
-    <div className="m-5 bg-slate-200 h-fit w-52">
+    <div className="m-5 bg-slate-200 h-fit w-52 rounded-lg">
       <h3 className="p-5 font-semibold">{title}</h3>
       <AddCardButton />
     </div>
